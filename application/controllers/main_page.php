72d3e173ad7466f07a3b42f15f494d16873611b4
Flonka <flonkation@gmail.com>
2013-01-15 15:47:25 +0100

    Another good commit.

--- application/controllers/main_page.php
+++ application/controllers/main_page.php
@@ -1,31 +1,29 @@
 <?php
 class Main_page extends CI_Controller {
-
+	
 	public function index()
 	{
 
-		_load_main_view();
-
-/*
-		if (_is_logged_in() === FALSE)
+		$this->_load_main_view();
+		
+		/*
+		if ($this->_is_logged_in() === FALSE)
 		{
 			redirect('login', 'refresh');
 		}
 		else
 		{
-			_load_main_view();
+			$this->_load_main_view();
 		}
 		*/
 	}
 
 	private function _load_main_view()
 	{
-		$data['username'] = $this->session->userdata('logindata')['username'];
-
-		$this->load->view('main_view', $data);
+		$this->load->view('main_view');
 	}
 
-	private bool function _is_logged_in()
+	private function _is_logged_in()
 	{
 		if ($this->session->userdata('logindata') === FALSE)
 		{
