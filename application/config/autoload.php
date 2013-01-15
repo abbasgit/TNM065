d64f5fbd00ffeff4ac63f860b11191f146776ab3
Flonka <flonkation@gmail.com>
2013-01-15 13:21:47 +0100

    Added libraries and helpers to autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,3 +1,4 @@
+
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------
@@ -52,7 +53,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session');
+$autoload['libraries'] = array('database','session', 'form_validation');
 
 
 /*
@@ -64,7 +65,7 @@ $autoload['libraries'] = array('database','session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'form');
 
 
 /*
