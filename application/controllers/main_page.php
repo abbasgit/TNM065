3db7f69959e7bf63ed274a04fb850f9cefcb2db0
Flonka <flonkation@gmail.com>
2013-01-15 15:03:26 +0100

    Main_page changes!

--- application/controllers/main_page.php
+++ application/controllers/main_page.php
@@ -3,7 +3,38 @@ class Main_page extends CI_Controller {
 
 	public function index()
 	{
-		echo 'Main page here.....';
+		if (_is_logged_in() === FALSE)
+		{
+			redirect('login', 'refresh');
+		}
+		else
+		{
+			echo 'Main page here.....';
+		}
+		
+	}
+
+	private bool function _is_logged_in()
+	{
+
+		if ($this->session->userdata('logindata') === FALSE)
+		{
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+
+	}
+
+	public function logout()
+	{
+
+		//Clears the current session.
+		$this->session->sess_destroy();
+
+		redirect('home_controller', 'refresh');
 	}
 }
 
