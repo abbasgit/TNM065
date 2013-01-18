2dd0fd78664515c42801ac5fffea0e8339b7f53c
Flonka <flonkation@gmail.com>
2013-01-18 10:51:34 +0100

    Navbar!

---
+++ application/views/navigation_menu.php
@@ -0,0 +1,13 @@
+<div id="navcontainer">
+	<ul id="navlist">
+		<li>
+			<a href="#">Report</a>
+		</li>
+		<li>
+			<a href="#">Profile</a>
+		</li>
+		<li id="logout">
+			<a href="main_page/logout">Logout</a>
+		</li>
+	</ul>
+</div>
