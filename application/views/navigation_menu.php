5f234fa6344700889729b74db50cb963b000e34f
Flonka <flonkation@gmail.com>
2013-01-19 19:25:30 +0100

    Visual changes in navigation bar.

--- application/views/navigation_menu.php
+++ application/views/navigation_menu.php
@@ -7,6 +7,7 @@
 			<a id="profile">Profile</a>
 		</li>
 		<li id="logout">
+			<span class="logged_in_user">Logged in as: <?php echo $username; ?></span>
 			<a href="<?php echo base_url(); ?>index.php?/main_page/logout">Logout</a>
 		</li>
 	</ul>
